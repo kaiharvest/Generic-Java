1d2ab9244a12b4105df637cd2b23d82e8d761f88
indra <indradwiprabowo27@gmail.com>
2025-04-20 15:19:19 +0700

    lanjutan project

--- src/Generic/Pair.java
+++ src/Generic/Pair.java
@@ -1,7 +1,28 @@
 package Generic;
 
-public class Pair {
+public class Pair<T, U> {
 
+	private T first;
+	private U secound;
 
+	public Pair(T first, U secound) {
+		this.first = first;
+		this.secound = secound;
+	}
 
+	public T getFirst() {
+		return first;
+	}
+
+	public void setFirst(T first) {
+		this.first = first;
+	}
+
+	public U getSecound() {
+		return secound;
+	}
+
+	public void setSecound(U secound) {
+		this.secound = secound;
+	}
 }
